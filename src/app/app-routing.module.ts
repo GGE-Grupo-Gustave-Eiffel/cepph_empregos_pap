4891d2dd467546c859d2e641147b63c3f43cabf6
Luís Tchitue <ls04af@gmail.com>
2022-04-17 16:32:05 +0100

    criamos um novo componente (Home), que carrega as vagas disponiveis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+
+  {path : '', component : HomeComponent},
 
   {path:'login', loadChildren : () => import('./login/login.module').then(m => m.LoginModule)},
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 
 import { RequisicaoService } from './servico/requisicao.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
